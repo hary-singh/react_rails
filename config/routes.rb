e30af988dd9d74e19683f93c54a0d1fb67b3d3df
Harry Dhillon <navbirsdhillon@gmail.com>
2021-03-22 19:08:18 -0600

    routes updated and client added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   # no longer for navigation, api call, api routes
   namespace :api do 
-    resources :todos
+    resources :todos 
+    # resources :todos do
+    #   resources :comments
+    # end
   end
+
 end
